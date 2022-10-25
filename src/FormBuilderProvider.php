d5a3d73b110199d75b7ccf303a43ee65369d6560
Adam Lee <adam@codelabx.ltd>
2022-10-25 12:15:20 +0100

    Initial commit

---
+++ src/FormBuilderProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace XtendLunar\Features\FormBuilder;
+
+use CodeLabX\XtendLaravel\Base\XtendFeatureProvider;
+
+class FormBuilderProvider extends XtendFeatureProvider
+{
+    /**
+     * Extends register service provider
+     *
+     * @return void
+     */
+    public function withRegister(): void
+    {
+        //
+    }
+
+    /**
+     * Extends boot service provider
+     *
+     * @return void
+     */
+    public function withBoot(): void
+    {
+        //
+    }
+}
